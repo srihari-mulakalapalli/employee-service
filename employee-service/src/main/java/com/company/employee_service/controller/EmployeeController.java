1720225a94f03670247601c8d98d397ad1261767
Ubuntu <ubuntu@ip-10-80-2-233.ec2.internal>
2026-06-17 08:29:46 +0000

    Added controller

--- employee-service/src/main/java/com/company/employee_service/controller/EmployeeController.java
+++ employee-service/src/main/java/com/company/employee_service/controller/EmployeeController.java
@@ -1,5 +1,13 @@
 package com.company.employee_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Employee Service Running";
+    }
 }
