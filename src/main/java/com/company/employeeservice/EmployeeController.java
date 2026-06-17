bbf7823c835464b88b02d57c536086dd657553b3
Ubuntu <ubuntu@ip-10-80-2-233.ec2.internal>
2026-06-17 13:26:06 +0000

    Added

---
+++ src/main/java/com/company/employeeservice/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.company.employeeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee Service Running";
+    }
+
+    @GetMapping("/employees")
+    public String employees() {
+        return "Employee List";
+    }
+}
